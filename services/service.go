d858f4a53ca2446981ac3e3bc8ce3a67d5163862
luke_aul <auliya.lukman@sigma.co.id>
2026-04-04 23:31:02 +0700

    add Auth to service layer

--- services/service.go
+++ services/service.go
@@ -3,8 +3,11 @@ package services
 import dbora "oraluke.com/conn-ora1/db"
 
 type Services struct {
+	*AuthenticationService
 }
 
 func NewServices(db *dbora.OraDBInstance) *Services {
-	return &Services{}
+	return &Services{
+		AuthenticationService: NewAuthentication(db),
+	}
 }
